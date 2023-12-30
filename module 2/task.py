fe13a956925eec84167dda2d16fb5d21ec6d6ebb
HarshitSarvaiya09 <harshitprajapati926@gmail.com>
2023-12-30 16:51:30 +0530

    task

---
+++ module 2/task.py
@@ -0,0 +1,6 @@
+data=[]
+a=int(input("how many student do you want add:"))
+for i in range(a):
+    x=input("enter student name :")
+data.append(x)
+print(data)
---
+++ module 2/tasl1.py
@@ -0,0 +1,10 @@
+data=set()
+n=int(input("how any student you want :"))
+
+for i in range(n):
+
+    
+    x=input("enter student name :")
+    data.add(x)
+
+    print(data)
